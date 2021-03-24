e7fc15daf91547de2e193df43bd99b9643d7b4b5
james <james_d02@protonmail.com>
2021-03-24 22:28:09 +0000

    Updated

--- src/ReferenceBuilderTemplate.py
+++ src/ReferenceBuilderTemplate.py
@@ -1,11 +1,24 @@
+import os
 from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('src/templates'))
 
-
 class ReferenceBuilderTemplate:
     def __init__(self, files, dictionary):
+        self.__createReferenceDirectory()
         self.files = files
         self.dictionary = dictionary
+
+
+    def __createReferenceDirectory(self):
+        path = "./docs/references"
+        if(os.path.exists(path) == False):
+            try:
+                os.mkdir(path)
+            except OSError:
+                print ("Creation of the directory %s failed" % path)
+            else:
+                print ("Successfully created the directory %s " % path)
+
 
     def createIndex(self, fileNames):
         template = env.get_template('reference_index.html')
